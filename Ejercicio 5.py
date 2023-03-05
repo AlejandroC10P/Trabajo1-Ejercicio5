294c76fe8bca761a25b6f861d081f85fe9d2a356
Alejandro Castro <111087081+AlejandroC10P@users.noreply.github.com>
2023-03-04 19:13:57 -0500

    Ejercicio 5

---
+++ Ejercicio 5.py
@@ -0,0 +1,7 @@
+Suma = 0 #Valor asignado inicialmente a la variable
+X = 20 #Valor asignado inicialmente a la variable
+Suma = Suma + X #Se reemplaza el valor de la variable 'Suma', siendo ahora 20
+Y = 40
+X = X + Y ** 2 #Se reemplaza el valor de la variable 'X', siendo ahora 1620
+Suma = Suma + (X/Y) #Se reemplaza nuevamente el valor de la variable 'Suma', siendo ahora 60.5
+print('El valor de la suma es', Suma)
